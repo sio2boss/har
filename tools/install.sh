149885d5051c698ab857ee1b9d1ab37beb2735c5
otto <otto@sitemetric.com>
2019-09-08 16:00:27 -0400

    Improving verboseness of install script

--- tools/install.sh
+++ tools/install.sh
@@ -28,12 +28,19 @@ case $OS in
   *) ;;
 esac
 
-echo "https://github.com/sio2boss/har/releases/download/${VERSION}/har-${VERSION}-${OSARCH}.${FORMAT}"
+echo "Installing Har ${VERSION}..."
+echo "  * Using https://github.com/sio2boss/har/releases/download/${VERSION}/har-${VERSION}-${OSARCH}.${FORMAT}"
 curl -o /tmp/har.${FORMAT} -fsSL https://github.com/sio2boss/har/releases/download/${VERSION}/har-${VERSION}-${OSARCH}.${FORMAT}
 
 if [ -e /tmp/har.${FORMAT} ]; then
-    rm -f /usr/local/bin/har && cd /usr/local/bin/ && tar xfz /tmp/har.${FORMAT} && rm /tmp/har.${FORMAT} && echo "done"
+    rm -f /usr/local/bin/har && \
+      echo "  * Removing existing executable" && \
+      cd /usr/local/bin/ && \
+      tar xfz /tmp/har.${FORMAT} && \
+      echo "  * Extracting" && \
+      rm /tmp/har.${FORMAT} && \
+      echo "  * Success!"
     exit
 fi
 
-echo "Failed due to some reason. Please try manually downloading har and copy the binary to /usr/local/bin"
+echo "  * Failed due to some reason. Please try manually downloading har and copy the binary to /usr/local/bin"
